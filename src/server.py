1be12b7230589d4480806e00272cdc50cb034068
Vinay Kumar <vinay@codecrux.com>
2020-01-09 03:20:41 +0530

    sfds

--- src/server.py
+++ src/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Hasura"
+    return "Hello World from Hasura haha "
 
 if __name__ == "__main__":
   app.config['TEMPLATES_AUTO_RELOAD'] = True
